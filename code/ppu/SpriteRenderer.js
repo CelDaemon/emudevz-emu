ced1b5dd58e28484d34da6d22fca0540c1395967
CelDaemon <celdaemon@voidgroup.net>
2025-12-08 14:16:54 +0100

    🖥️ PPU - 🗜️ Mapper tick
    Level: ppu-mapper-tick

--- code/ppu/SpriteRenderer.js
+++ code/ppu/SpriteRenderer.js
@@ -1,5 +1,6 @@
 import Sprite from '/lib/ppu/Sprite';
 import Tile from 'Tile';
+import byte from '/lib/byte';
 
 const SPRITE_SIZE = 4;
 
@@ -10,6 +11,8 @@ const SPRITE_X = 3;
 
 const TILE_SIZE = 8;
 
+const FB_WIDTH = 256;
+
 export default class SpriteRenderer {
   constructor(ppu) {
     this.ppu = ppu;
@@ -18,7 +21,7 @@ export default class SpriteRenderer {
   renderScanline() {
     if(!this.ppu.registers.ppuMask.showSprites)
       return;
-    const sprites = this._evaluate();
+    const sprites = this._evaluate().reverse();
     this._render(sprites);
   }
 
@@ -34,12 +37,12 @@ export default class SpriteRenderer {
       }
       sprites.push(sprite);
     }
-
     return sprites.reverse();
-    
+
   }
 
   _render(sprites) {
+    const attempts = new Uint8Array(FB_WIDTH / 8);
     const y = this.ppu.scanline;
     for(const sprite of sprites) {
       const offsetY = sprite.diffY(y);
@@ -47,15 +50,20 @@ export default class SpriteRenderer {
       const colorY = sprite.flipY ? 7 - tileY : tileY;
       const tile = new Tile(this.ppu, sprite.patternTableId, sprite.tileIdFor(offsetY), colorY);
       for(let offsetX = 0; offsetX < 8; offsetX++) {
+        // TODO fix sprite masking through priority quirk
         const x = sprite.x + offsetX;
+        const attemptByteIndex = Math.floor(x / 8);
+        const attemptBitIndex = x % 8;
+        if(byte.getFlag(attempts[attemptByteIndex], x % 8))
+          continue;
         if(!this.ppu.registers.ppuMask.showSpritesInFirst8Pixels && x < 8)
           continue;
         const colorX = sprite.flipX ? 7 - offsetX : offsetX;
         const colorIndex = tile.getColorIndex(colorX);
         if(colorIndex == 0)
           continue;
+        attempts[attemptByteIndex] = byte.setBit(attempts[attemptByteIndex], attemptBitIndex, true);
         const color = this.ppu.getColor(sprite.paletteId, colorIndex);
-        
         if(this.ppu.registers.ppuMask.showBackground && this.ppu.isBackgroundPixelOpaque(x, y)) {
           if(sprite.id == 0)
             this.ppu.registers.ppuStatus.sprite0Hit = 1;
@@ -70,10 +78,10 @@ export default class SpriteRenderer {
   _createSprite(id) {
     const ppuCtrl = this.ppu.registers.ppuCtrl;
     const oamRam = this.ppu.memory.oamRam;
-    
+
     const address = id * SPRITE_SIZE;
     const y = oamRam[address + SPRITE_Y] + 1;
-    
+
     const attributes = oamRam[address + SPRITE_ATTRIBUTES];
     const x = oamRam[address + SPRITE_X];
 
@@ -84,7 +92,7 @@ export default class SpriteRenderer {
 
     const patternTableId = 
       isLarge ? oamRam[address + SPRITE_TILE_ID] & 0x1 : ppuCtrl.sprite8x8PatternTableId;
-    
+
     return new Sprite(id, x, y, this.ppu.registers.ppuCtrl.spriteSize == 1, patternTableId, tileId, attributes);
   }
 }
