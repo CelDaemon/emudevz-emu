2cdac49aaf5a1cfb37983bab87715f0d9f07028c
CelDaemon <celdaemon@voidgroup.net>
2025-12-08 14:16:53 +0100

    Level: cartridge-the-magic-constant

--- code/Cartridge.js
+++ code/Cartridge.js
@@ -1,2 +1,15 @@
+const MAGIC = "NES";
+
+
 export default class Cartridge {
+  /** @type {Uint8Array} */
+  bytes;
+  constructor(bytes) {
+    if(bytes[0] != MAGIC.charCodeAt(0) ||
+       bytes[1] != MAGIC.charCodeAt(1) ||
+       bytes[2] != MAGIC.charCodeAt(2) ||
+       bytes[3] != 0x1A)
+      throw new Error("Invalid ROM");
+    this.bytes = bytes;
+  }
 }
