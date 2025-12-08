7013c77581d6920da6458156d5fce82f1ebaf0e6
CelDaemon <celdaemon@voidgroup.net>
2025-12-08 14:16:54 +0100

    🔊 APU - 📏 Triangle Channel (2/3): Regular length counter
    Level: apu-triangle-channel-2-3-regular-length-counter

--- code/apu/TriangleChannel.js
+++ code/apu/TriangleChannel.js
@@ -1,3 +1,4 @@
+import LengthCounter from 'LengthCounter';
 import TriangleOscillator from '/lib/apu/TriangleOscillator';
 import byte from '/lib/byte';
 
@@ -8,19 +9,33 @@ export default class TriangleChannel {
     this.apu = apu;
     this.registers = this.apu.registers.triangle;
 
-    this.oscillator = new TriangleOscillator();
+    this.previousSample = 0;
 
-    this.timer = 0;
+    this.oscillator = new TriangleOscillator();
+    this.lengthCounter = new LengthCounter();
   }
 
   sample() {
+    if(!this.isEnabled() || !this.lengthCounter.isActive())
+      return this.previousSample;
     const timer = byte.buildU16(
       this.registers.timerHighLCL.timerHigh,
       this.registers.timerLow.value
     );
     if(timer < 2 || timer > 0x7FF)
-      return 0;
+      return this.previousSample = 0;
     this.oscillator.frequency = CPU_FREQ / (16 * (timer + 1)) / 2;
-    return this.oscillator.sample();
+    return this.previousSample = this.oscillator.sample();
+  }
+
+  isEnabled() {
+    return !!this.apu.registers.apuControl.enableTriangle;
+  }
+
+  quarterFrame() {
+  }
+
+  halfFrame() {
+    this.lengthCounter.clock(this.isEnabled(), this.registers.lengthControl.halt);
   }
 }
