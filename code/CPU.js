49744f57ccd756d1c2119065e8ecfc4bd220e851
CelDaemon <celdaemon@voidgroup.net>
2025-12-08 14:16:53 +0100

    Level: cpu-flags

--- code/CPU.js
+++ code/CPU.js
@@ -1,4 +1,4 @@
-class Register {
+class ArrayRegister {
   constructor(wrapper) {
     this.wrapper = wrapper;
   }
@@ -12,16 +12,46 @@ class Register {
 }
 
 
-class Register8Bit extends Register {
+class Register8Bit extends ArrayRegister {
   constructor() {
     super(new Uint8Array(1));
   }
   
 }
 
-class Register16Bit extends Register {
+class Register16Bit extends ArrayRegister {
   constructor() {
     super(new Uint16Array(1));
+  }
+}
+
+class FlagsRegister {
+  constructor() {
+    this.c = false;
+    this.z = false;
+    this.i = false;
+    this.d = false;
+    this.v = false;
+    this.n = false;
+  }
+
+  getValue() {
+    return this.c |
+      this.z << 1 |
+      this.i << 2 |
+      this.d << 3 |
+      1 << 5 |
+      this.v << 6 |
+      this.n << 7;
+  }
+
+  setValue(value) {
+    this.c = (value & 1) != 0;
+    this.z = (value >> 1 & 1) != 0;
+    this.i = (value >> 2 & 1) != 0;
+    this.d = (value >> 3 & 1) != 0;
+    this.v = (value >> 6 & 1) != 0;
+    this.n = (value >> 7 & 1) != 0;
   }
 }
 
@@ -38,5 +68,7 @@ export default class CPU {
     this.sp = new Register8Bit();
     
     this.pc = new Register16Bit();
+
+    this.flags = new FlagsRegister();
   }
 }
