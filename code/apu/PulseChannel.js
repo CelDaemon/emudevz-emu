1852c4593bdf07244e6eb33814b227bb6ea03242
CelDaemon <celdaemon@voidgroup.net>
2025-12-08 14:16:54 +0100

    🔊 APU - 📉 Pulse Channels (4/5): Volume envelope
    Level: apu-pulse-channels-4-5-volume-envelope

--- code/apu/PulseChannel.js
+++ code/apu/PulseChannel.js
@@ -1,6 +1,7 @@
 import byte from "/lib/byte";
 import PulseOscillator from '/lib/apu/PulseOscillator';
 import LengthCounter from 'LengthCounter';
+import VolumeEnvelope from 'VolumeEnvelope';
 
 const CPU_FREQ = 1789773;
 
@@ -16,6 +17,7 @@ export default class PulseChannel {
 
     this.oscillator = new PulseOscillator();
     this.lengthCounter = new LengthCounter();
+    this.volumeEnvelope = new VolumeEnvelope();
 
     this.previousSample = 0;
   }
@@ -25,7 +27,9 @@ export default class PulseChannel {
       return this.previousSample;
     this.oscillator.frequency = CPU_FREQ / (16 * (this.timer + 1));
     this.oscillator.dutyCycle = this.apu.registers.pulses[this.id].control.dutyCycleId;
-    this.oscillator.volume = this.apu.registers.pulses[this.id].control.volumeOrEnvelopePeriod;
+    this.oscillator.volume = this.apu.registers.pulses[this.id].control.constantVolume ?
+      this.apu.registers.pulses[this.id].control.volumeOrEnvelopePeriod :
+      this.volumeEnvelope.volume;
     this.previousSample = this.oscillator.sample();
     return this.previousSample;
   }
@@ -46,6 +50,8 @@ export default class PulseChannel {
   }
 
   quarterFrame() {
+    const pulseControl = this.apu.registers.pulses[this.id].control;
+    this.volumeEnvelope.clock(pulseControl.volumeOrEnvelopePeriod, pulseControl.envelopeLoopOrLengthCounterHalt);
   }
 
   halfFrame() {
