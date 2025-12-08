9e21d0de023b2a23c9cb0e9f2a2c7dbad0f82997
CelDaemon <celdaemon@voidgroup.net>
2025-12-08 14:16:53 +0100

    💾 Cartridge - 🗣️ Reading the header
    Level: cartridge-reading-the-header

--- code/Cartridge.js
+++ code/Cartridge.js
@@ -1,15 +1,59 @@
 const MAGIC = "NES";
+const PRG_ROM_SIZE_OFFSET = 4;
+const CHR_ROM_SIZE_OFFSET = 5;
+const FLAGS_OFFSET = 6;
+const MAPPER_LOWER_OFFSET = FLAGS_OFFSET;
+const MAPPER_UPPER_OFFSET = 7;
 
+const FLAG_FOUR_SCREEN = 0b1000;
+const FLAG_PADDING = 0b100;
+const FLAG_PRG_RAM = 0b10;
+const FLAG_MIRRORING_VERTICAL = 0b1;
+
+
+const MIRRORING_VERTICAL = "VERTICAL";
+const MIRRORING_HORIZONTAL = "HORIZONTAL";
+const MIRRORING_FOUR_SCREEN = "FOUR_SCREEN";
+
+function getMirroringId(flags) {
+  if((flags & FLAG_FOUR_SCREEN) != 0)
+    return MIRRORING_FOUR_SCREEN;
+  if((flags & FLAG_MIRRORING_VERTICAL) != 0)
+    return MIRRORING_VERTICAL;
+  return MIRRORING_HORIZONTAL;
+}
 
 export default class Cartridge {
-  /** @type {Uint8Array} */
-  bytes;
   constructor(bytes) {
-    if(bytes[0] != MAGIC.charCodeAt(0) ||
-       bytes[1] != MAGIC.charCodeAt(1) ||
-       bytes[2] != MAGIC.charCodeAt(2) ||
-       bytes[3] != 0x1A)
+    if(bytes[0] !== MAGIC.charCodeAt(0) ||
+       bytes[1] !== MAGIC.charCodeAt(1) ||
+       bytes[2] !== MAGIC.charCodeAt(2) ||
+       bytes[3] !== 0x1A)
       throw new Error("Invalid ROM");
+    
     this.bytes = bytes;
+    
+    const prgRomPages = bytes[PRG_ROM_SIZE_OFFSET];
+    const chrRomPages = bytes[CHR_ROM_SIZE_OFFSET];
+    const usesChrRam = chrRomPages === 0;
+    
+    const flags = bytes[FLAGS_OFFSET];
+    
+    const has512BytePadding = (flags & FLAG_PADDING) != 0;
+    const hasPrgRam = (flags & FLAG_PRG_RAM) != 0;
+    const mirroringId = getMirroringId(flags);
+    
+    const mapperId = bytes[MAPPER_UPPER_OFFSET] & 0b11110000 |
+      bytes[MAPPER_LOWER_OFFSET] >> 4;
+    
+    this.header = {
+      prgRomPages,
+      chrRomPages,
+      usesChrRam,
+      has512BytePadding,
+      hasPrgRam,
+      mirroringId,
+      mapperId
+    };
   }
 }
